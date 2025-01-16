64b13dce1b011fafd19a88b3759fb97e9e88f590
Stepan Rad <stepanrad@MacBook-Air-Stepan.local>
2025-01-16 21:51:04 +0200

    gddkf

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const app = express();
-const cors = require('cors');
+// const cors = require('cors');
 
 // app.use((req, res, next) => {
 //   // res.header('Access-Control-Allow-Origin', '*');
@@ -14,13 +14,13 @@ const cors = require('cors');
 
 // app.options('*', cors(corsOptions)); 
 
-const corsOptions = {
-    origin:'*', 
-    credentials:true,
-    // optionSuccessStatus:200
-}
+// const corsOptions = {
+//     origin:'*', 
+//     credentials:true,
+//     // optionSuccessStatus:200
+// }
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
